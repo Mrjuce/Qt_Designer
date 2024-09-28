b31cadc536a431f29cb03f524b0726ca0a5d58c5
Mrjuce <matkbozhykalexnder@gmail.com>
2024-09-29 00:36:09 +0300

    Update notes_main.py

--- smart_notes_alexander/notes_main.py
+++ smart_notes_alexander/notes_main.py
@@ -58,7 +58,7 @@ convert_to_text = QPushButton()
 convert_to_text.setText("Конвертувати до txt")
 
 actoin_theme_btn = QPushButton()
-actoin_theme_btn.setText("Змінити тему ")
+actoin_theme_btn.setText("Змінити тему на чорну")
 
 
 #------------------------
@@ -69,7 +69,7 @@ row1 = QHBoxLayout()
 row1.addWidget(make_note)
 row1.addWidget(delete_note)
 
-row2 = QVBoxLayout()
+row2 = QHBoxLayout()
 row2.addWidget(add_to_note)
 row2.addWidget(unpin_to_note)
 
@@ -85,11 +85,13 @@ col2.addWidget(save_note)
 col2.addWidget(QLabel("Список тегів:"))
 col2.addWidget(list_widget_2)
 col2.addWidget(QLabel("Пошук данних:"))
-col2.addWidget(search_for_tag)
-col2.addWidget(search_for_text)
+col2.addWidget(tag_searcher)
+col2.addWidget(text_searcher)
 col2.addLayout(row2)
 col2.addWidget(search_for_tag)
 col2.addWidget(search_for_text)
+
+
 col2.addWidget(convert_to_text)
 col2.addWidget(actoin_theme_btn)
 
@@ -102,24 +104,27 @@ layout_note.addLayout(col2)
 window.setLayout(layout_note)
 
 #------------------------
-#           
+#          кольора
 #------------------------
-list_widget_1.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-list_widget_1.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-delete_note.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-make_note.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-save_note.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-list_widget_2.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-search_for_tag.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-search_for_text.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-add_to_note.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-unpin_to_note.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-search_for_tag.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-search_for_text.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-convert_to_text.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
-actoin_theme_btn.setStyleSheet("background: rgb(0 , 255, 255); font-size:15px")
 
 
+list_widget_1.setStyleSheet("background: rgb(235, 235, 235); font-size:17px")# поле списку заміток
+delete_note.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")#видалити замітку
+make_note.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# створити замітку
+save_note.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# зберегти замітку
+list_widget_2.setStyleSheet("background: rgb(235, 235, 235); font-size:17px")# поле списку тегів
+search_for_tag.setStyleSheet("background: rgb(235, 235, 235); font-size:17px;border: 1.9px solid #834F7C;")# поле пошуку за тегом
+search_for_text.setStyleSheet("background: rgb(235, 235, 235); font-size:17px;border: 1.9px solid #834F7C;")#поле пошуку за текстом
+add_to_note.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# додати тег до замітки
+unpin_to_note.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# вікріпити тег від заміток
+search_for_tag.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# шукати за тегом 
+search_for_text.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# шукати за текстом
+convert_to_text.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")# конвертувати до txt
+actoin_theme_btn.setStyleSheet("background: rgb(48, 152, 255); font-size:17px;border: 1.9px solid #834F7C;")#змінити тему на чорну
+tag_searcher.setStyleSheet("background: rgb(235, 235, 235); font-size:17px;border: 1.9px solid #834F7C;")  #введіть тег
+text_searcher.setStyleSheet("background: rgb(235, 235, 235); font-size:17px;border: 1.9px solid #834F7C;") #введіть текст
+
+text_editor.setStyleSheet("background: rgb(235, 235, 235); font-size:17px;border: 1.9px solid #834F7C;")   #введіть текс замітки 
 
 window.show()
 app.exec_()
